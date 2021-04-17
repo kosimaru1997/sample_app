27cacfe82076679aadf00ae3601cb06fd586646b
kosimaru1997 <kosimaru1997@gmail.com>
2021-04-17 11:22:57 +0000

    create password-resetForm

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -10,8 +10,21 @@ class UserMailerPreview < ActionMailer::Preview
   end
 
   # Preview this email at
-  # http://localhost:3000/rails/mailers/user_mailer/password_reset
+  # http://50338cf9e221479187f757aabceaf2ad.vfs.cloud9.us-east-1.amazonaws.com/rails/mailers/user_mailer/password_reset
   def password_reset
-    UserMailer.password_reset
+    user = User.first
+    user.reset_token = User.new_token
+    UserMailer.password_reset(user)
+  end
+  
+  test "password_reset" do
+    user = users(:michael)
+    user.reset_token = User.new_token
+    mail = UserMailer.password_reset(user)
+    assert_equal "Password reset", mail.subject
+    assert_equal [user.email], mail.to
+    assert_equal ["noreply@example.com"], mail.from
+    assert_match user.reset_token,        mail.body.encoded
+    assert_match CGI.escape(user.email),  mail.body.encoded
   end
 end
