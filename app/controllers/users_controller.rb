225d0eb8997f96e640fb9613662b40d9919e52e5
kosimaru1997 <kosimaru1997@gmail.com>
2021-04-18 17:40:36 +0000

    フィード（画像を除く）完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,14 +56,6 @@ class UsersController < ApplicationController
 
   # beforeアクション
 
-    # ログイン済みユーザーかどうか確認
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
 
   def correct_user
     @user = User.find(params[:id])
